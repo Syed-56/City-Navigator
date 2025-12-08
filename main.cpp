4fbb2941a017f6f2ed8775ab59e774e8a682fb8a
Syed-56 <ssa.56.sultan@gmail.com>
2025-12-08 09:18:06 +0500

    Added BG audio

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 //to compile:
-// g++ main.cpp introScreen.cpp sampleCity.cpp createCity.cpp travelFunc.cpp -o City-Navigator.exe -I"C:/SFML-2.6.1/include" -L"C:/SFML-2.6.1/lib" -lsfml-graphics -lsfml-window -lsfml-system (type .cpp file names whom we are coding here we did introscreen and samplecity and main will always be compiled)
+// g++ main.cpp introScreen.cpp sampleCity.cpp createCity.cpp travelFunc.cpp -o City-Navigator.exe -I"C:/SFML-2.6.1/include" -L"C:/SFML-2.6.1/lib" -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include "introScreen.h"
 #include "sampleCity.h"
+#include <iostream>
 
 int main()
 {
@@ -11,6 +13,22 @@ int main()
 
     // Create fullscreen window
     sf::RenderWindow window(desktop, "City Navigator", sf::Style::Fullscreen);
+
+    // ============ MUSIC SETUP ============
+    sf::Music backgroundMusic;
+    
+    // Try to load background music
+    if (backgroundMusic.openFromFile("song.mp3")) {
+        // Set music properties
+        backgroundMusic.setLoop(true);
+        backgroundMusic.setVolume(50.f);
+        backgroundMusic.play();
+        std::cout << "Background music started.\n";
+    } 
+    else {
+        std::cerr << "Warning: Could not load song.mp3\n";
+    }
+    // ====================================
 
     // Initialize global textures
     loadBuildingTextures();
@@ -24,8 +42,13 @@ int main()
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+            
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Escape) {
+                    window.close();
+                }
+            }
 
-            // Pass events to intro screen
             intro.handleEvent(event, window);
         }
 
